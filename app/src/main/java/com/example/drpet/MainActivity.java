f68d9c1a0fae59cd661b52c5184c50344ee8e5ba
khushi07 <khushiverma988@gmail.com>
2020-06-02 17:12:31 -0400

    first commit

--- app/src/main/java/com/example/drpet/MainActivity.java
+++ app/src/main/java/com/example/drpet/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Ayub's commit in project");
+        System.out.println("khushi commit in project");
+
     }
    // new commit testing
 
