6431de2c983fdd784dbf363345c28957aec8e849
soorajsuneesh <soorajsuneesh@gmail.com>
2020-06-04 15:00:03 -0400

    menu items

--- app/src/main/java/com/example/drpet/MenuFragment.java
+++ app/src/main/java/com/example/drpet/MenuFragment.java
@@ -1,19 +1,86 @@
 package com.example.drpet;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+
 public class MenuFragment extends Fragment {
+    private GoogleMap googleMap;
+    private MapView mapView;
+    private boolean mapsSupported = true;
+
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        super.onCreate(savedInstanceState);
+        MapsInitializer.initialize(getActivity());
+
+        if (mapView != null) {
+            mapView.onCreate(savedInstanceState);
+        }
+        initializeMap();
+    }
+
+    private void initializeMap() {
+        if (googleMap == null && mapsSupported) {
+            mapView = (MapView) getActivity().findViewById(R.id.map);
+            googleMap = mapView.getMap();
+            //setup markers etc...
+
+    }}
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+//        return inflater.inflate(R.layout.fragment_menu, container, false);
+        final RelativeLayout parent = (RelativeLayout) inflater.inflate(R.layout.fragment_menu, container, false);
+        mapView = (MapView) parent.findViewById(R.id.map);
+        return parent;
+
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+        initializeMap();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
