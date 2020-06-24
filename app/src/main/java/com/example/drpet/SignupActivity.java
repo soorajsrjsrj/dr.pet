bab8072b3301b8e5837fcf6dc7d70fc0606c5549
khushi07 <khushiverma988@gmail.com>
2020-06-23 23:27:54 -0400

    Added SignupActivity.java

--- app/src/main/java/com/example/drpet/SignupActivity.java
+++ app/src/main/java/com/example/drpet/SignupActivity.java
@@ -2,14 +2,13 @@ package com.example.drpet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
     }
 }
