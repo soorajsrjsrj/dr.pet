c186a062a94d0a0bd189f0ae8f26db0af59d47fe
khushi07 <khushiverma988@gmail.com>
2020-07-08 17:45:53 -0400

    Did keyup setting on card preview

--- app/src/main/java/com/example/drpet/Payment.java
+++ app/src/main/java/com/example/drpet/Payment.java
@@ -5,10 +5,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,10 +25,13 @@ public class Payment extends Fragment {
 
     private DBManager dbManager;
 
+    private TextWatcher text = null;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_payment, container, false);
+
     }
 
     @Override
@@ -36,9 +42,12 @@ public class Payment extends Fragment {
 //        Cursor cursor = dbManager.();
 
         final TextView c_Name = (TextView) getView().findViewById(R.id.card_name);
-        final TextView c_Number = (TextView) getView().findViewById(R.id.card_number);
+        final EditText c_Number = (EditText) getView().findViewById(R.id.card_number);
         final TextView c_expiryDate = (TextView) getView().findViewById(R.id.expiry_date);
         final TextView c_cvvNumb = (TextView) getView().findViewById(R.id.cvc);
+        final TextView txt_input = (TextView) getView().findViewById(R.id.card_preview_number);
+        final TextView txt_name = (TextView) getView().findViewById(R.id.card_preview_name);
+        final TextView txt_date = (TextView) getView().findViewById(R.id.card_preview_expiry);
         Button payButton = (Button) getView().findViewById(R.id.btn_pay);
 
         SharedPreferences pref = getActivity().getApplicationContext().getSharedPreferences("id_pref", Context.MODE_PRIVATE);
@@ -51,8 +60,6 @@ public class Payment extends Fragment {
 //            expiryDate.setText(cursor.getString(2));
 //            cvvNumb.setText(cursor.getString(3));
 //        }
-//
-//
 
         payButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,13 +71,35 @@ public class Payment extends Fragment {
                 String cvv = c_cvvNumb.getText().toString();
 
                 dbManager.insertintopayment(cardName, cardNumber, expiry, cvv, user_id);
-                Toast.makeText(Payment.this.getActivity(), "Succesfully Added" + cardName + ":" + cardNumber +
-                        " : " + user_id , Toast.LENGTH_LONG).show();
+                Toast.makeText(Payment.this.getActivity(), cardNumber + " Successfully Added", Toast.LENGTH_LONG).show();
 
                 getFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new MenuFragment()).commit();
             }
         });
+
+        text = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+               txt_input.setText(c_Number.getText().toString());
+               txt_name.setText(c_Name.getText().toString());
+               txt_date.setText(c_expiryDate.getText().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        };
+
+        c_Number.addTextChangedListener(text);
+        c_Name.addTextChangedListener(text);
+        c_expiryDate.addTextChangedListener(text);
     }
 
 }
